94f7c0d4e548465d4a2d9b1303902b2ccb7c42d9
Shiny1711 <220701267@rajalakshmi.edu.in>
2024-10-23 14:05:20 +0530

    Add files via upload

---
+++ Day 3/pract1.js
@@ -0,0 +1,24 @@
+// console.log('hello world')
+
+// var a = 100;
+// var b = 100;
+// var c = a+b;
+// console.log("Addition of 2 numbers is : "+c);
+
+var km=10;
+var miles=km*0.621;
+console.log(km+" km is equal to "+miles+" miles");
+
+var kg = 10;
+var pounds = kg * 2.2;
+console.log(kg+" kg is equal to "+pounds+" pounds");
+
+var m = 56;
+var cm = 100 * m;
+console.log(m+" m is equal to "+cm);
+
+var p = 50000;
+var n = 2;
+var r = 0.05;
+var si = (p*n*r)/100;
+console.log("The Simple interest "+si);
