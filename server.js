3caa10a6c78134e0ea9429b592aefe549e7d716e
Shiny1711 <220701267@rajalakshmi.edu.in>
2024-10-24 11:43:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-//welcome studentssssssssssssss
+const http1 = require('http');//calling the weserver protocol = http
+const webserver=http1.createServer((req,res)=>{//create the web server
+    const smodule = require("./smodule");
+    smodule("Shiny Alex");
+    res.end("Welcome world"); //response to the client
+});
+webserver.listen(2222,()=> //assign the port number
+{
+    
+    console.log("The server running under the port number: http://localhost:2222");//for inform to the user for running our app
+});
